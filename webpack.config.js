d23a40e616bb337941e35b492ffdb8d7c729d681
Mark N Carpenter Jr <mark@mncjr.com>
2021-04-20 02:44:11 -0400

    BUFIX: Build pipeline deps
    Updated webpack so it actually builds

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['@babel/env']
           }
         }
       },
@@ -30,5 +30,15 @@ module.exports = {
   },
   externals: {
     'react': 'commonjs react'
+  },
+  resolve: {
+    fallback: {
+      "stream": require.resolve("stream-browserify"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "url": require.resolve("url/"),
+      "buffer": require.resolve("buffer/"),
+      "timers": require.resolve("timers-browserify")
+    }
   }
 };
